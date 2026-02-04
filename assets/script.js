27d98332d21611a7d0698e508471628460320d69
tpdigitalx <vk18x11@gmail.com>
2026-02-04 13:03:04 +0530

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// Highlight active nav link based on current page
+(() => {
+  const path = (location.pathname.split("/").pop() || "index.html").toLowerCase();
+  document.querySelectorAll(".menu a").forEach(a => {
+    const href = (a.getAttribute("href") || "").toLowerCase();
+    if (href === path) a.classList.add("active");
+  });
+})();
