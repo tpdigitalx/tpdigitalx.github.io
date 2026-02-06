f42a15cb9cf6c5010c56a43db6145979a8467a66
tpdigitalx <vk18x11@gmail.com>
2026-02-06 11:18:46 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,1 @@
-// Highlight active nav link based on current page
-(() => {
-  const path = (location.pathname.split("/").pop() || "index.html").toLowerCase();
-  document.querySelectorAll(".menu a").forEach(a => {
-    const href = (a.getAttribute("href") || "").toLowerCase();
-    if (href === path) a.classList.add("active");
-  });
-})();
+
